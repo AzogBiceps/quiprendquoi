32adbefbb9804796934eb52ecb67dad305a7ebc7
Rayan Adamczak <ag-hiro@hotmail.fr>
2020-03-16 17:11:04 +0100

    11

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const app = express()
-
+const app = express();
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Qui prend quoi ?' });
 });
 
 app.post('/party', function(req, res) {
+  console.log(req.body);
   res.send('Post ok !');
 });
 
